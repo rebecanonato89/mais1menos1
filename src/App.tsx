8a9dc2ca4ffaba28137a23157247fa3652c846af
rebecanonato89 <rebecanonato89@gmail.com>
2022-04-06 15:22:22 -0300

    first Commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import logo from "./logo.svg";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Bem vindo ao <b>Mais 1 Menos 1</b>!
+        </p>
+        <p>.</p>
+        <p>Acompanhe nossa rede social</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
